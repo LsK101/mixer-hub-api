d9e0011b7f5a0bb6fc593ceaf0c026398d631076
Patrick Panahon <patrick.panahon@gmail.com>
2018-02-05 15:21:25 -0500

    CORS

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
- const express = require('express');
- const app = express();
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const {CLIENT_ORIGIN} = require('./config');
 
- const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
- app.get('/api/*', (req, res) => {
-   res.json({ok: true});
- });
+app.use(
+	cors({
+ 		origin: CLIENT_ORIGIN
+ 	})
+);
 
- app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+app.get('/api/*', (req, res) => {
+	res.json({ok: true});
+});
 
- module.exports = {app};
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+
+module.exports = {app};
