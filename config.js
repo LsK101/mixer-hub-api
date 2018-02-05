a7565431ab79fd15e0d03bae6f3bd665baa062a2
Patrick Panahon <patrick.panahon@gmail.com>
2018-02-05 18:41:21 -0500

    cors url added

--- config.js
+++ config.js
@@ -1,2 +1,3 @@
 exports.PORT = process.env.PORT || 5000;
 exports.DATABASE_URL = process.env.DATABASE_URL
+exports.CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || 'https://goofy-neumann-b87ae4.netlify.com/'
